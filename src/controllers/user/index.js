540b80dac1e7d94bb101b4ea34ad400703cf3c64
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-12 15:20:15 -0300

    feat(index.js) exporta todos os controladores relacionados a usuarios

---
+++ src/controllers/user/index.js
@@ -0,0 +1,11 @@
+const { createUser } = require('./createUser');
+const { userLogin } = require('./userLogin');
+const { detailUser } = require('./detailUser');
+const { updateUser } = require('./updateUser');
+
+module.exports = {
+  createUser,
+  userLogin,
+  detailUser,
+  updateUser
+};
