316f9a2f25c3876ca62fa46a3ff7c63d6fa2588a
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-12 15:22:01 -0300

    feat(index.js) exporta todos os repositorios relacionados relacionados a rota de gerar extrato

---
+++ src/repositories/transactions/transactionHistory/index.js
@@ -0,0 +1,9 @@
+const { getEnter } = require('./getEnter');
+const { getExit } = require('./getExit');
+const { transactionFilter } = require('./transactionFilter');
+
+module.exports = {
+  getEnter,
+  getExit,
+  transactionFilter
+};
