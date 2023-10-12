718721efe601d6a2512eeb8dab858484449ca712
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-12 15:22:13 -0300

    feat(index.js) exporta todos os repositorios relacionados relacionados a rota de atualizar transacao

---
+++ src/repositories/transactions/updateTransaction/index.js
@@ -0,0 +1,7 @@
+const { updateTransaction} = require('./updateTransaction');
+const { updateVerifyer } = require('./updateVerifyer');
+
+module.exports = {
+  updateTransaction,
+  updateVerifyer
+};
